0fe333c9ef27da6aab07d9df262413213c645f74
LinhnhiFPTU <joliebui2002@gmail.com>
2024-01-03 13:51:45 +0700

    change api getlink

--- src/app-services/services.js
+++ src/app-services/services.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 const LoadData = () => {
-    return axios.get("https://659379ecbb1297071990a049.mockapi.io/api/v1/app/todos/task");
+    return axios.get("internal-lab3tier-alb-app-180975476.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos");
 }
 const addData = (str)=>{
-    return axios.post("https://659379ecbb1297071990a049.mockapi.io/api/v1/app/todos/task",{content:str});
+    return axios.post("internal-lab3tier-alb-app-180975476.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos",{content:str});
 } 
 
 const deleteData = (id)=>{
-    return axios.delete("https://659379ecbb1297071990a049.mockapi.io/api/v1/app/todos/task/" + id);
+    return axios.delete("internal-lab3tier-alb-app-180975476.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos" + id);
 } 
 
 
