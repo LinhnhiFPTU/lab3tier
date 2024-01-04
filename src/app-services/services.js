5f286dcdce7bed7add56e5c58bebe5d7b007b764
LinhnhiFPTU <joliebui2002@gmail.com>
2024-01-04 13:40:34 +0700

    change api link v2

--- src/app-services/services.js
+++ src/app-services/services.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 const LoadData = () => {
-    return axios.get("internal-lab3tier-alb-app-180975476.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos");
+    return axios.get("lab3tier-alb-app-2-745997772.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos");
 }
 const addData = (str)=>{
-    return axios.post("internal-lab3tier-alb-app-180975476.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos",{content:str});
+    return axios.post("lab3tier-alb-app-2-745997772.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos",{content:str});
 } 
 
 const deleteData = (id)=>{
-    return axios.delete("internal-lab3tier-alb-app-180975476.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos" + id);
+    return axios.delete("lab3tier-alb-app-2-745997772.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos" + id);
 } 
 
 
