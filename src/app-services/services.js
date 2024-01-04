94dc11f90a313e9b511024c80664fb32e85be663
LinhnhiFPTU <joliebui2002@gmail.com>
2024-01-04 17:29:31 +0700

    get api link v3

--- src/app-services/services.js
+++ src/app-services/services.js
@@ -1,14 +1,14 @@
 import axios from "axios"
 
 const LoadData = () => {
-    return axios.get("lab3tier-alb-app-2-745997772.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos");
+    return axios.get("https://qfu0uf8srk.execute-api.us-east-1.amazonaws.com/api/v1/app/todos");
 }
 const addData = (str)=>{
-    return axios.post("lab3tier-alb-app-2-745997772.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos",{content:str});
+    return axios.post("https://qfu0uf8srk.execute-api.us-east-1.amazonaws.com/api/v1/app/todos",{content:str});
 } 
 
 const deleteData = (id)=>{
-    return axios.delete("lab3tier-alb-app-2-745997772.us-east-1.elb.amazonaws.com:4200/api/v1/app/todos" + id);
+    return axios.delete("https://qfu0uf8srk.execute-api.us-east-1.amazonaws.com/api/v1/app/todos/" + id);
 } 
 
 
